00978905b49ad308eea350c0ec5bd85e64c30d97
dpres <thepressu@gmail.com>
2017-08-23 14:24:01 +0200

    menu icons

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-    $(".button-collapse").sideNav();    
+
+    iconMenu=0;
+	$(".icon-menu").click(function(){
+		if (iconMenu++%2) {
+            $(".pas:eq(0) , .pas:eq(3)").css("opacity", "1");
+			$(".pas:eq(1) , .pas:eq(2)").css("transform", "rotate(0deg)");
+			$(".pas:eq(1) , .pas:eq(2)").css("margin-top", "2px");
+			$(".pas:eq(1) , .pas:eq(2)").css("width", "22px");
+			$(".icon-menu").css("padding-top", "3px");
+		}else{
+            $(".pas:eq(0) , .pas:eq(3)").css("opacity", "0");
+			$(".pas").eq(1).css("transform", "rotate(45deg)");
+			$(".pas").eq(2).css("transform", "rotate(-45deg)");
+            $(".pas").eq(1).css("margin-top", "6px");
+            $(".pas").eq(2).css("margin-top", "-2px");
+			$(".pas:eq(1) , .pas:eq(2)").css("width", "24px");
+			$(".icon-menu").css("padding-top", "0px");
+		}
+    });
+});
+
+$(window).scroll(function() {
+    if ($(".fixed-menu").offset().top > 50) {
+        $(".fixed-menu").css( "background-color", "rgba(0, 0, 0, 0.9)" );
+    }else{
+        $(".fixed-menu").css( "background-color", "transparent" );
+    }
 });
