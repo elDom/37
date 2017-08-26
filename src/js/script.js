1eeb574ddec74e4afa3ae9331260b35998703a97
dpres <thepressu@gmail.com>
2017-08-26 16:11:18 +0200

    owl carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,59 @@
-jQuery(window).on("load",function(){
+jQuery(window).on("load", function () {
     jQuery(".preloader").fadeOut();
 });
 
-$(window).on("scroll load",function() {
+$(window).on("scroll load", function () {
     if ($(".fixed-menu").offset().top > 200) {
-        $(".fixed-menu").css( "background-color", "rgba(90, 0, 20, 1)" );
-    }else{
-        $(".fixed-menu").css( "background-color", "transparent" );
+        $(".fixed-menu").css("background-color", "rgba(90, 0, 20, 0.6)");
+    } else {
+        $(".fixed-menu").css("background-color", "transparent");
     }
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    iconMenu=0;
-	$(".icon-menu").click(function(){
-		if (iconMenu++%2) {
+    iconMenu = 0;
+    $(".icon-menu").click(function () {
+        if (iconMenu++ % 2) {
             $(".pas:eq(0) , .pas:eq(3)").css("opacity", "1");
-			$(".pas:eq(1) , .pas:eq(2)").css("transform", "rotate(0deg)");
-			$(".pas:eq(1) , .pas:eq(2)").css("margin-top", "2px");
-			$(".pas:eq(1) , .pas:eq(2)").css("width", "22px");
-			$(".icon-menu").css("padding-top", "3px");
+            $(".pas:eq(1) , .pas:eq(2)").css("transform", "rotate(0deg)");
+            $(".pas:eq(1) , .pas:eq(2)").css("margin-top", "2px");
+            $(".pas:eq(1) , .pas:eq(2)").css("width", "22px");
+            $(".icon-menu").css("padding-top", "3px");
             //when unclicked
             $(".mobile-menu").css("height", "0vh");
             $(".mobile-menu").css("background", "transparent");
-		}else{
+        } else {
             $(".pas:eq(0) , .pas:eq(3)").css("opacity", "0");
-			$(".pas").eq(1).css("transform", "rotate(45deg)");
-			$(".pas").eq(2).css("transform", "rotate(-45deg)");
+            $(".pas").eq(1).css("transform", "rotate(45deg)");
+            $(".pas").eq(2).css("transform", "rotate(-45deg)");
             $(".pas").eq(1).css("margin-top", "6px");
             $(".pas").eq(2).css("margin-top", "-2px");
-			$(".pas:eq(1) , .pas:eq(2)").css("width", "24px");
-			$(".icon-menu").css("padding-top", "0px");
+            $(".pas:eq(1) , .pas:eq(2)").css("width", "24px");
+            $(".icon-menu").css("padding-top", "0px");
             //when clicked
             $(".mobile-menu").css("height", "100vh");
             $(".mobile-menu").css("background", "#5a0014");
-		}
+        }
     });
 
-    $(".mobile-menu a").click(function(){
+    $(".mobile-menu a").click(function () {
         $(".icon-menu").click();
     });
 
     $('.scrollspy').scrollSpy({
-        scrollOffset:100,
+        scrollOffset: 100,
     });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 3,
+        center: true,
+        nav: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+
+    })
+
 });
